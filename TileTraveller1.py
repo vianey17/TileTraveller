b49391fd269db97b03238f5c6aca9a46d65d91e8
Vianey17 <vianey17@ru.is>
2018-09-21 22:46:15 +0000

    Should work, though not efficient without functions

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -15,22 +15,82 @@ position = 0
 switch = False
 
 while switch==False:
-    position = str(input("Direction:"))
-     
-    if position == "n":
-        y += 1
-        print(x,y)
-    elif position == "e":
-        x += 1
-        print(x,y)
-    elif position == "s":
-        y -= 1
-        print(x,y)
-    elif position == "w":
-        x -= 1
-        print(x,y)
-    else:
-        "Uuuunacceptable!"
+    
+    if (x==1 and y ==1) or (x==3 and y ==1):
+        print("You can travel: (N)orth")
+        position = str(input("Direction:"))
+        if position == "n":
+            y += 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+    
+    elif (x==1 and y==2):
+        print("You can travel: (N)orth or (E)ast")
+        position = str(input("Direction:"))
+        if position == "n":
+            y += 1
+            print(x,y)
+        elif position == "e":
+            x += 1
+            print(x,y)
+        elif position == "s":
+            y -= 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+    
+    elif (x==1 and y == 3):
+        print("You can travel: (S)outh or (E)ast")
+        position = str(input("Direction:"))
+        
+        if position == "e":
+            x += 1
+            print(x,y)
+        elif position == "s":
+            y -= 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+    
+    elif (x==2 and y == 3):
+        print("You can travel: (S)outh or (E)ast")
+        position = str(input("Direction:"))
+        
+        if position == "e":
+            x += 1
+            print(x,y)
+        elif position == "w":
+            x -= 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+    
+    elif (x==2 and y==2) or (x == 3 and y ==3):
+        print("You can travel: (S)outh or (W)est")
+        position = str(input("Direction:"))
+
+        if position == "s":
+            y -= 1
+            print(x,y)
+        elif position == "w":
+            x -= 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+    elif (x==3 and y==2):
+        print("You can travel: (N)orth or (S)outh")
+        position = str(input("Direction:"))
+
+        if position == "s":
+            y -= 1
+            print(x,y)
+        elif position == "n":
+            y += 1
+            print(x,y)
+        else:
+            print("Not a valid direction!")
+
     if x==2 and y == 1:        
         print("Victory!")
         switch = True
