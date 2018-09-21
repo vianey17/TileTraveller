815e6b8e841efcd1ad83dbe28a6bb024b3f39208
Vianey17 <vianey17@ru.is>
2018-09-21 21:23:59 +0000

    Initial thoughts

---
+++ TileTraveller1.py
@@ -0,0 +1,20 @@
+""" 
+You can travel: (N)orth.
+Direction: s
+Not a valid direction!
+Direction: n
+Victory! 
+"""
+# Create cardinal directions
+n, e = 1,1
+s, w = -1,-1
+
+#Initial position
+x = 1
+y = 1
+
+
+while x!=2 and y==1:
+    print("It worked!")
+    x = 2
+    y =1
